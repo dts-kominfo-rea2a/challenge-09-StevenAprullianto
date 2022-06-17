5c791dadcbe5a938391ee9a17b81aa065d047499
Steven Aprullianto <stevenaprullianto@gmail.com>
2022-06-17 22:21:13 +0700

    Challenge-09

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
-  return (
-    <div className="App">
-      
-    </div>
+  
+    return (
+      <div className="App" style={{flexDirection:'column'}}>
+        <Header title="Call a Friend" subtitle='your friendly contact app' />
+        <Contact name={contacts[0].name} phoneNumber={contacts[0].phone} email={contacts[0].email} image={contacts[0].photo} />
+        <Contact name={contacts[1].name} phoneNumber={contacts[1].phone} email={contacts[1].email} image={contacts[1].photo} />
+        <Contact name={contacts[2].name} phoneNumber={contacts[2].phone} email={contacts[2].email} image={contacts[2].photo} />
+        <Contact name={contacts[3].name} phoneNumber={contacts[3].phone} email={contacts[3].email} image={contacts[3].photo} />
+        <Contact name={contacts[4].name} phoneNumber={contacts[4].phone} email={contacts[4].email} image={contacts[4].photo} />
+        <Contact name={contacts[5].name} phoneNumber={contacts[5].phone} email={contacts[5].email} image={contacts[5].photo} />
+      </div>
   );
+  
+    
+  
+  
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
 
-const Header = () => {
+const Header = ({title, subtitle}) => {
     return (
-        <></>
+        <div style={{margin:'0 auto'}}>
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+            <p style={{color:'#fd938f'}}>*************************************************************************************************************</p>
+        </div>
     )
 }
 
