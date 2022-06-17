805cf0bd0807427f351f4ccdb70e3c9ed39cd546
Steven Aprullianto <stevenaprullianto@gmail.com>
2022-06-17 22:31:52 +0700

    Challenge-09

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = ({name, phoneNumber, email, image}) => {
     return (
         <div style={{display:'flex', flexDirection:'row', backgroundColor:'#f4deb3', borderRadius:'10px', width:'30%', height:'auto', margin:'0 auto', marginBottom:'20px'}}>
             <div style={{width:'50%',  display:'flex', verticalAlign:'middle', alignItems:'center', justifyContent:'center'}}>
-                <img src={image} alt="kitten" style={{borderRadius:'50%', width:'40%'}} />
+                <img src={image} alt="" style={{borderRadius:'50%', width:'40%'}} />
             </div>
             <div style={{width:'50%', margin:'0 auto'}}>
                 <h4>{name}</h4>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
 
-const Header = ({title, subtitle}) => {
+const Header = () => {
     return (
         <div style={{margin:'0 auto'}}>
-            <h1>{title}</h1>
-            <p>{subtitle}</p>
+            <h1>Call a Friend</h1>
+            <p>your friendly contact app</p>
             <p style={{color:'#fd938f'}}>*************************************************************************************************************</p>
         </div>
     )
