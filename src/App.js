db8b9fcb238bad11f3d1cf5af41321a347f6dfc7
Steven Aprullianto <stevenaprullianto@gmail.com>
2022-06-17 22:43:23 +0700

    Challenge-09

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ const App = () => {
   
     return (
       <div className="App" style={{flexDirection:'column'}}>
-        <Header title="Call a Friend" subtitle='your friendly contact app' />
-        <Contact name={contacts[0].name} phoneNumber={contacts[0].phone} email={contacts[0].email} image={contacts[0].photo} />
-        <Contact name={contacts[1].name} phoneNumber={contacts[1].phone} email={contacts[1].email} image={contacts[1].photo} />
-        <Contact name={contacts[2].name} phoneNumber={contacts[2].phone} email={contacts[2].email} image={contacts[2].photo} />
-        <Contact name={contacts[3].name} phoneNumber={contacts[3].phone} email={contacts[3].email} image={contacts[3].photo} />
-        <Contact name={contacts[4].name} phoneNumber={contacts[4].phone} email={contacts[4].email} image={contacts[4].photo} />
-        <Contact name={contacts[5].name} phoneNumber={contacts[5].phone} email={contacts[5].email} image={contacts[5].photo} />
+        <Header />
+        <Contact data={contacts[0]} />
+        <Contact data={contacts[1]} />
+        <Contact data={contacts[2]} />
+        <Contact data={contacts[3]} />
+        <Contact data={contacts[4]} />
+        <Contact data={contacts[5]} />
       </div>
   );
   
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,16 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = ({name, phoneNumber, email, image}) => {
+const Contact = ({data}) => {
     return (
         <div style={{display:'flex', flexDirection:'row', backgroundColor:'#f4deb3', borderRadius:'10px', width:'30%', height:'auto', margin:'0 auto', marginBottom:'20px'}}>
             <div style={{width:'50%',  display:'flex', verticalAlign:'middle', alignItems:'center', justifyContent:'center'}}>
-                <img src={image} alt="" style={{borderRadius:'50%', width:'40%'}} />
+                <img src={data.photo} alt="" style={{borderRadius:'50%', width:'40%'}} />
             </div>
             <div style={{width:'50%', margin:'0 auto'}}>
-                <h4>{name}</h4>
-                <p>{phoneNumber}</p>
-                <p>{email}</p>
+                <h4>{data.name}</h4>
+                <p>{data.phone}</p>
+                <p>{data.email}</p>
             </div>
         </div>
     )
